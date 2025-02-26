7cb11dc40cc75df4c9ad5a65487694c167101b64
Malcolm Schongalla <malcolm.schongalla@gmail.com>
2025-02-25 22:55:41 -0500

    Local users added via james-cli

--- images/james/conf/temp-cli-enable.sh
+++ images/james/conf/temp-cli-enable.sh
@@ -2,6 +2,7 @@
 #Helper script for image development, gets james-cli.sh prepared
 #Helpful for troubleshooting
 
-cp /app/james-project/server/apps/spring-app/target/appassembler/bin/james-cli.sh /app/james/conf/james-cli.sh
+ln /app/james-project/server/apps/spring-app/target/appassembler/bin/james-cli.sh /app/james/conf/james-cli.sh
+chmod +x /app/james-project/server/apps/spring-app/target/appassembler/bin/james-cli.sh
 chmod +x /app/james/conf/james-cli.sh
 cat /app/james/conf/jmxremote.password
