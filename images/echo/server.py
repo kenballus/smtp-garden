cd0fb637707fc78bbfd3ea75bba280a0053e64e2
Malcolm Schongalla <malcolm.schongalla@gmail.com>
2025-05-06 17:50:27 -0400

    Echo advertises ESMTP codes
    Mostly for the '250-PIPELINING' announcement.
    Echo latency may be a limiting factor in high-volume testing.
    Enabling pipelining may improve throughput.  Also, having it
    announce ESMTP features, as an experimental option, may expand
    test coverage.  Set EHLO_announce = False, to disable.

--- images/echo/server.py
+++ images/echo/server.py
@@ -1,6 +1,8 @@
 """
 SMTP Garden - Python Echo Server
 
+Version 1.5, -replies with ESMTP codes if it receives an "EHLO".
+             -set global EHLO_announce to False to disable
 Version 1.4, -replies with minimum 4 bytes (instead of 3) to appease finicky clients
              -generate_response method drafted in comment block -mss 8/27/2024
 Version 1.3, -handles ConnectionResetError, commonly caused by MTA disconnecting abruptly
@@ -14,12 +16,19 @@ import asyncio
 import signal
 import sys
 
+EHLO_announce = True
 RECV_SIZE = 65536
 
 """
 # Alternate response generator, if higher fidelity to normal behavior is needed
 # (<CR><LF>.<CR><LF> detection is not ideal)
 def generate_response(client_bytes, is_datamode):
+    EHLO_block = b"250-echo.smtp.garden\r\n" + \
+                 b"250-PIPELINING\r\n" + \
+                 b"250-SIZE " + str(RECV_SIZE).encode('ascii') + b"\r\n" + \
+                 b"250-VRFY\r\n" + \
+                 b"250-8BITMIME\r\n" + \
+                 b"250 CHUNKING"
     if is_datamode:
         if client_bytes == b".":
             return b"250 Ok", False
@@ -29,11 +38,19 @@ def generate_response(client_bytes, is_datamode):
         return b"354 Send data\r\n", True
     elif client_bytes == b"QUIT":
         return b"221 Bye\r\n", False
+    elif client_bytes.lstrip()[:4].upper() == b"EHLO":
+        return EHLO_block, False
     else:
         return b"250 Ok", False
 """
 
 async def handle_connection(reader, writer):
+    EHLO_block = b"250-echo.smtp.garden\r\n" + \
+                 b"250-PIPELINING\r\n" + \
+                 b"250-SIZE " + str(RECV_SIZE).encode('ascii') + b"\r\n" + \
+                 b"250-VRFY\r\n" + \
+                 b"250-8BITMIME\r\n" + \
+                 b"250 CHUNKING\r\n"
     try:
         writer.write(b"220 echo ESMTP\r\n")
         await writer.drain()
@@ -46,7 +63,9 @@ async def handle_connection(reader, writer):
             print(f"Received from {peer_host}:{peer_port}: {data!r}")
             sys.stdout.flush()
 #            response, is_datamode = generate_response(data.lstrip()[:5].rstrip().upper(), is_datamode)
-            response = b"354 \r\n" if data.lstrip()[:5].rstrip().upper() == b"DATA" else b"250 \r\n"
+            response = b"354 \r\n" if data.lstrip()[:5].rstrip().upper() == b"DATA" else \
+                       EHLO_block if data.lstrip()[:4].upper() == b"EHLO" and EHLO_announce else \
+                       b"250 \r\n"
             writer.write(response)
             await writer.drain()
         writer.close()
